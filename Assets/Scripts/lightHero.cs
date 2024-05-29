d886ada09521fbfe381ff6c91029924b8de48a59
PequenoBananinha <cenatotesserolli@gmail.com>
2024-05-28 21:56:03 -0300

    UpdateAula

--- Assets/Scripts/lightHero.cs
+++ Assets/Scripts/lightHero.cs
@@ -1,58 +1,41 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ligthHero : MonoBehaviour
 {
     public GameObject[] players;
-    public GameObject blackScreen;
-    public float visibilityStartTime = 130f; // Tempo para começar a tela preta a ficar visível (2:10 minutos)
-    public float visibilityDuration = 20f; // Duração da visibilidade da tela preta (20 segundos)
-    public float effectDuration = 10f; // Duração do efeito
+    public Image imagem;
+    public float opacidadeInicial = 0f;
+    public float opacidadeFinal = 1f;
 
-    private bool eventStarted = false;
-    private float currentTime;
-    private bool blackScreenVisible = false;
+    public float tempoDecorrido = 0f;
 
-    void Start()
+
+    private void Start()
     {
-        currentTime = visibilityStartTime + visibilityDuration; // Começa invisível
-        SetBlackScreenVisibility(false);
-    }
 
+        if (imagem != null)
+        {
+            Color cor = imagem.color;
+
+            cor.a = opacidadeInicial;
+
+            imagem.color = cor;
+        }
+    }
     void Update()
     {
-        currentTime -= Time.deltaTime;
+        tempoDecorrido += Time.deltaTime;
 
-        if (!eventStarted && currentTime <= visibilityStartTime)
+        if(tempoDecorrido >= 5f) 
         {
-            SetBlackScreenVisibility(true); // Torna a tela preta visível
-            eventStarted = true;
+            Color cor = imagem.color;
+
+            cor.a = opacidadeFinal;
+
+            imagem.color = cor;
         }
-
-        if (eventStarted && currentTime <= 0)
-        {
-            EndEvent();
-        }
-    }
-
-    void EndEvent()
-    {
-        // Desativar efeito de luz nos jogadores
-        foreach (GameObject player in players)
-        {
-            Light playerLight = player.GetComponent<Light>();
-            if (playerLight != null)
-            {
-                playerLight.enabled = false;
-            }
-        }
-
-        eventStarted = false;
-        SetBlackScreenVisibility(false); // Torna a tela preta invisível
-    }
-
-    void SetBlackScreenVisibility(bool visible)
-    {
-        blackScreen.SetActive(visible);
-        blackScreenVisible = visible;
     }
 }
+
+

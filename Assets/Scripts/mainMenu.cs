187f956760b398772b21d2ff5c24cd2196e98a39
Renato <cenatotesserolli@gmail.com>
2024-06-02 23:49:26 -0300

    menu(2/4)

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
     public void playButton()
     {
-        
+        SceneManager.LoadScene(2);
+    }
+
+    public void quitButton()
+    {
+        Application.Quit();
     }
 }

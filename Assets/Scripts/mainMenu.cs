90e118f97f97e70f336335e6c8ac64686189c0eb
Renato <cenatotesserolli@gmail.com>
2024-06-02 23:14:25 -0300

    menu (1/4)

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenu : MonoBehaviour
+{
+    public void playButton()
+    {
+        
+    }
+}
